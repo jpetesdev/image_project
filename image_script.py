5a5e9c64bc0a3a98c13d6354d2ff4c675bbe622a
jpetesdev <55292817+jpetesdev@users.noreply.github.com>
2023-02-11 17:40:56 -0600

    Update image_script.py

--- image_script.py
+++ image_script.py
@@ -2,7 +2,6 @@
 
 import os
 import PIL
-import glob
 from os import listdir
 from PIL import Image
 
@@ -10,19 +9,19 @@ with Image.open("./images/ic_local_pizza_black_48dp") as im:
         im.rotate(90).show()
 
 image_dir = "./images"
-
 output_dir = "new_images/"
 
-#images = glob.glob("./images/ic_*")
-
+#Iterates over each image in the folder they are stored in with the os module
 for image in os.listdir(image_dir):
         if image != ".DS_Store":
+                #Creates the full file path to image
                 inputPath = os.path.join(image_dir, image)
+                #Uses the Image module from PIL to open the image and then manipulate it.
                 img = Image.open(inputPath)
-                #print(image)
-                #print("Image exists")
+                #Creates the new folder that the images will be stored in, but with the same file name
                 fullOutPath = os.path.join(output_dir, image)
                 img = img.rotate(-90)
                 img = img.resize((128, 128))
                 img = img.convert("RGB")
+                #Saves the image as a .jpeg from original TIFF format. Had to convert prior in order to do that. 
                 img = img.save(fullOutPath, format="JPEG")
